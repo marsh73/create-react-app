a3bac6677f5c835d516590af1265885d22223f8d
Justin Marshall <jmarshall@digitalocean.com>
2019-07-19 06:24:41 -0700

    testing

--- packages/react-scripts/template-typescript/src/App.tsx
+++ packages/react-scripts/template-typescript/src/App.tsx
@@ -1,23 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ImportIconSprite from 'walrus/build/components/ImportIconSprite';
+
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <ImportIconSprite
+        url={`https://ui.digitalocean.com/1.0.0/icons.svg`}
+      />
       </header>
     </div>
   );
